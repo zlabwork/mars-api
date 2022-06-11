a00d8083752c6ff070dffc50cc9ee67791e4ba2d
Joe <joe@xxtime.com>
2022-06-11 23:30:37 +0800

    init

---
+++ src/test/java/dev/zlab/mars/MarsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zlab.mars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
